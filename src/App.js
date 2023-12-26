685a87145b07d68ff4f378143902b7091e122710
Yingying Wang <yxw241@gmail.com>
2023-12-26 18:32:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+//contexts
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,20 +17,39 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		//Context API part is not yet implemented,check if the item already in the card
+		const existingItem = cart.find(cartItem => cartItem.id === item.id);
+
+		if(existingItem) {
+			// If the item is already in the cart, update its quantity
+			setCart(cart.map(cartItem =>  {
+				if(cartItem.id === item.id) {
+					return {...cartItem, quantity: cartItem.quantity + 1};
+				}
+				return cartItem;
+			}));
+		}else {      // If the item is not in the cart, add it with a quantity of 1
+			setCart([...cart, {...item, quantity: 1}])
+		}
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+
+       <ProductContext.Provider value={{ products, addItem }}>
+	      <CartContext.Provider value={{ cart, setCart }}>
+			     <Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+                 <Products />
+            </Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart  />
 			</Route>
+			</CartContext.Provider>
+			   </ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,5 @@
+//this context is going to be utilized by our ShoppingCart and Navigation component.
+
+import { createContext } from "react";
+
+export const CartContext = createContext({});
